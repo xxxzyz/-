03d5552bfca7050e800acb1d10f4669573febe5f
xxx <xxx19980316@163.com>
2018-11-04 19:07:12 +0800

    ToolBar的使用

--- day4_toolbar/src/main/java/com/example/xxx/day4_toolbar/MainActivity.java
+++ day4_toolbar/src/main/java/com/example/xxx/day4_toolbar/MainActivity.java
@@ -1,13 +1,68 @@
 package com.example.xxx.day4_toolbar;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+        setSupportActionBar(toolbar);
+
+        toolbar.setLogo(R.mipmap.ic_launcher);
+
+        // 显示应用的Logo
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+
+        // 显示标题和子标题
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        toolbar.setTitle("ToolbarDemo");
+        toolbar.setSubtitle("the detail of toolbar");
+
+        // 显示导航按钮
+        toolbar.setNavigationIcon(R.drawable.ic_launcher_background);
+
+
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.toolbar_action1:
+                // do something
+                return true;
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+
+    private void initView() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+    }
+
 }
